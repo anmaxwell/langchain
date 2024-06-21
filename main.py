abe6e2774ea3c49111cedf4ad25e83c4702117be
Ania Maxwell <ania@MacBook-Pro-2.local>
2024-06-21 14:42:51 +0100

    build: integrated similarity functions

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from tour_details import get_tour_details
+from similarity_functions import return_response
+
 
 # read in the links of each tour
 tour_urls = []
@@ -9,7 +11,7 @@ with open('red_tours.txt', 'r') as file:
         tour_urls.append(tour.strip())
 
 # get the tour details from each link
-tours, contents, corpus_of_documents = get_tour_details(tour_urls)
+tours, corpus_of_documents = get_tour_details(tour_urls)
 
 # set up the streamlit UI with a title and a box to enter text
 st.title("Want some help choosing a holiday?")
@@ -20,11 +22,11 @@ with st.container():
         query = st.text_area("My ideal holiday is..", height= 100, key= "query_text")
         button = st.button("Submit", key="button")
 
-# send the input and corpus to the similarity functions
-relevant_tour, relevant_document = return_response(jaccard_similarity, query, corpus_of_documents)
+my_ouput = return_response(tours, corpus_of_documents, query)
 
 # return the output to the console
-st.write('The entered text is:', query)
+st.write('The entered text is:', corpus_of_documents)
+
 
 #test for checking output
 for tour in tours:
